0f16d4da46d7b77704522e7baf6bf77e04a641a4
Siva Prakash <prakashdeveloper@outlook.in>
2025-09-06 10:42:45 +0530

    Added encryption & decryption methods

--- 2FA/crypto/crypto.go
+++ 2FA/crypto/crypto.go
@@ -1,6 +1,8 @@
 package crypto
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
 	"fmt"
 	"io"
@@ -24,4 +26,43 @@ func GenerateSalt() ([]byte, error) {
 func GenerateKey(password, salt []byte) ([]byte, error) {
 
 	return scrypt.Key(password, salt, 32768, 8, 1, KeySize)
+}
+
+func Encrypt(data, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, fmt.Errorf("could not create new cipher: %w", err)
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, fmt.Errorf("could not create GCM: %w", err)
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, fmt.Errorf("failed to create nonce: %w", err)
+	}
+
+	return gcm.Seal(nonce, nonce, data, nil), nil
+}
+
+func Decrypt(data, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, fmt.Errorf("could not create new cipher: %w", err)
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, fmt.Errorf("could not create GCM: %w", err)
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(data) < nonceSize {
+		return nil, fmt.Errorf("ciphertext is too short")
+	}
+
+	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+	return gcm.Open(nil, nonce, ciphertext, nil)
 }
