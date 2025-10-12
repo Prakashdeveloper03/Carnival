0fa84f64c4767ee30bdb3e0b0e1efbc62bace3c0
Siva Prakash <prakashdeveloper@outlook.in>
2025-10-12 15:53:29 +0530

    Added application source for graphql

---
+++ fastapi-graphql-todo/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from strawberry.asgi import GraphQL
+from schema import schema
+
+app = FastAPI()
+graphql_app = GraphQL(schema)
+
+app.add_route("/graphql", graphql_app)
+app.add_websocket_route("/graphql", graphql_app)
